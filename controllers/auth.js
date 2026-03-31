b280172f7971886059b397a45362a56b6f249159
Andrés Machetá <sr.macheta08@gmail.com>
2026-03-31 14:15:22 -0500

    Connect to a DB in MongoDB Atlas

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,27 @@
 const { response } = require("express");
 
 const crearUsuario = (req, res = response) => {
-  res.json({
+  const { name, email, password } = req.body;
+
+  const errors = validationResult(req);
+
+  res.status(201).json({
     ok: true,
+    msg: "registro",
+    name,
+    email,
+    password,
   });
 };
 
 const loginUsuario = (req, res = response) => {
+  const { email, password } = req.body;
+
   res.json({
     ok: true,
+    msg: "login",
+    email,
+    password,
   });
 };
 
--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 require("dotenv").config();
+const { dbConnection } = require("./database/config");
 
 //Crear el servidor de express
 const app = express();
 
+//Base de datos
+dbConnection();
+
 //Directorio público
 app.use(express.static("public"));
+
+//Lectura y parseo del body
+
+app.use(express.json());
 
 //Rutas
 app.use("/api/auth", require("./routes/auth"));
