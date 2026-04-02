2a4301c61c0b8edb014d8e3ea1139e0aa61e09c3
Andrés Machetá <sr.macheta08@gmail.com>
2026-04-02 11:08:59 -0500

    Initial modeling of events routes

---
+++ routes/events.js
@@ -0,0 +1,25 @@
+const { Router } = require("express");
+const { validarJWT } = require("../middlewares/validar-jwt");
+const router = Router();
+const {
+  getEventos,
+  crearEvento,
+  actualizarEvento,
+  eliminarEvento,
+} = require("../controllers/events");
+
+//Todas pasan por la validación del token
+
+//Obtener eventos
+router.get("/", getEventos);
+
+//Crear un nuevo evento
+router.post("/", crearEvento);
+
+//Actualizar evento
+router.put("/:id", actualizarEvento);
+
+//Borrar evento
+router.delete("/:id", eliminarEvento);
+
+module.exports = router;
