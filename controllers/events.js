0c043f8c3e89a712429894172fa4c86f143d8c08
Andrés Machetá <sr.macheta08@gmail.com>
2026-04-02 12:18:19 -0500

    Add getEventos and createEventos functions

--- controllers/events.js
+++ controllers/events.js
@@ -1,17 +1,32 @@
 const { response } = require("express");
+const Evento = require("../models/Evento");
 
-const getEventos = (req, res = response) => {
+const getEventos = async (req, res = response) => {
+  const eventos = await Evento.find().populate("user", "name");
+
   res.json({
     ok: true,
-    msg: "getEventos",
+    msg: eventos,
   });
 };
 
-const crearEvento = (req, res = response) => {
-  res.json({
-    ok: true,
-    msg: "crearEvento",
-  });
+const crearEvento = async (req, res = response) => {
+  const evento = new Evento(req.body);
+
+  try {
+    evento.user = req.uid;
+    const eventoGuardado = await evento.save();
+    res.json({
+      ok: true,
+      evento: eventoGuardado,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
 };
 
 const actualizarEvento = (req, res = response) => {
--- routes/events.js
+++ routes/events.js
@@ -1,12 +1,15 @@
 const { Router } = require("express");
 const { validarJWT } = require("../middlewares/validar-jwt");
-const router = Router();
+const { check } = require("express-validator");
+const { isDate } = require("../helpers/isDate");
+const { validarCampos } = require("../middlewares/validar-campos");
 const {
   getEventos,
   crearEvento,
   actualizarEvento,
   eliminarEvento,
 } = require("../controllers/events");
+const router = Router();
 
 //Todas pasan por la validación del token
 router.use(validarJWT);
@@ -15,7 +18,18 @@ router.use(validarJWT);
 router.get("/", getEventos);
 
 //Crear un nuevo evento
-router.post("/", crearEvento);
+router.post(
+  "/",
+  [
+    check("title", "El título es obligatorio").not().isEmpty(),
+    check("start", "Fecha de inicio es obligatorio").custom(isDate),
+    check("end", "Fecha de inicio es obligatorio").custom(isDate),
+    // check('title', 'El título es obligatorio').not().isEmpty(),
+    validarCampos,
+  ],
+  validarCampos,
+  crearEvento,
+);
 
 //Actualizar evento
 router.put("/:id", actualizarEvento);
